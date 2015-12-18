96ae9a41b0ee86687f6a912499337879354d06b5
aepyornis <ziggy@elephant-bird.net>
2015-12-18 19:49:02 +0000

    use .data() instead of .datum()

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ d3.json('data/bushwick.json', function(err, bushwick){
   var boroughs = topojson.feature(bushwick, bushwick.objects.boroughs);
   var cd = topojson.feature(bushwick, bushwick.objects.cd);
 
-  
   var w = 700;
   var h = 800;  
   var svg = svgMaker(w,h, '#white-pop-map');
@@ -21,11 +20,15 @@ d3.json('data/bushwick.json', function(err, bushwick){
     .attr('fill-opacity', 0.8);
 
   //bushwick census
-  svg.append('path')
-    .datum(bushwickCensus)
-    .attr('d', path);
-    
-
+  svg.selectAll('path')
+    .data(bushwickCensus.features)
+    .enter()
+    .append('path')
+    .attr('d', path)
+    .attr('stroke', 'white')
+    .attr('fill', function(d){
+      return 'blue';
+    });
 
 }); //end of d3.json   
 
